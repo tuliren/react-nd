65c2a7a3e4c8f3cf5a3a62a8b8dfbe3a60102f70
Liren Tu <tuliren.git@outlook.com>
2017-07-26 00:13:39 -0700

    Add stateless functional component

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,5 +1,33 @@
 import React, { Component } from 'react'
 
+/**
+ * Stateless functional component
+ *
+ * When the component only has one render method and does not track interal state,
+ * it can be replaced with a stateless functional component which has props as its argument.
+ */
+
+// function ListContacts(props) {
+//   return (
+//     <ol className='contact-list'>
+//       {props.contacts.map((contact) => (
+//         <li key={contact.id} className='contact-list-item'>
+//           <div className='contact-avatar' style={{
+//             backgroundImage: `url(${contact.avatarURL})`
+//           }}/>
+//           <div className='contact-details'>
+//             <p>{contact.name}</p>
+//             <p>{contact.email}</p>
+//           </div>
+//           <button className='contact-remove'>
+//             Remove
+//           </button>
+//         </li>
+//       ))}
+//     </ol>
+//   )
+// }
+
 class ListContacts extends Component {
   render() {
     return (

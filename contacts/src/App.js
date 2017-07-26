570bde62bc2a648266996eb12302b112a381ae23
Liren Tu <tuliren.git@outlook.com>
2017-07-25 23:49:05 -0700

    Modify initial files

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,35 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts;
-
-    return <ol>
-      {people.map((person, index) => (
-          <li key={index}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          {name: 'Michael'},
-          {name: 'Ryan'},
-          {name: 'Tyler'}
-        ]}/>
-        <ContactList contacts={[
-          {name: 'Amanda'},
-          {name: 'Richard'},
-          {name: 'Geoff'}
-        ]}/>
+      <div>
+        Hello World!
       </div>
-    );
+    )
   }
 }
 

6634a1718451764740acde9af6d52addca90dba9
Liren Tu <tuliren.git@outlook.com>
2017-07-27 01:36:46 -0700

    Implement remove button

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -41,7 +41,7 @@ class ListContacts extends Component {
               <p>{contact.name}</p>
               <p>{contact.email}</p>
             </div>
-            <button className='contact-remove'>
+            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
               Remove
             </button>
           </li>

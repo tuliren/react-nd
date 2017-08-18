2ee559e1a21ff0bc9a67cb8be13a921d4f6553c6
Liren Tu <tuliren.git@outlook.com>
2017-08-17 23:05:49 -0700

    Use route to contral page navigation

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import ListContacts from './ListContacts';
 import CreateContact from './CreateContact';
 import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
   state = {
-    screen: 'list', // list, create
     contacts: []
   };
 
@@ -26,18 +26,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.screen === 'list' && (
+        <Route exact path='/' render={() => (
           <ListContacts
             onDeleteContact={this.removeContact}
-            onNavigate={() => {
-              this.setState({ screen: 'create' })
-            }}
             contacts={this.state.contacts}
           />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContact/>
-        )}
+        )}/>
+
+        <Route path='/create' component={CreateContact}/>
       </div>
     )
   }

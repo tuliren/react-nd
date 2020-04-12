857bedafa7acebcc3fa644ca663a7e6a08f09be5
Liren Tu <tuliren.git@outlook.com>
2020-04-12 16:05:24 -0700

    Remove console log

--- myreads/src/App.js
+++ myreads/src/App.js
@@ -16,7 +16,6 @@ class BooksApp extends React.Component {
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-      console.log(books);
       this.setState({ books });
     });
   }

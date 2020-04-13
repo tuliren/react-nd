c46142713158f5b898f0a37f5b881cc3eff031aa
Liren Tu <tuliren.git@outlook.com>
2020-04-12 18:33:37 -0700

    Auto focus on search input

--- myreads/src/SearchBooks.js
+++ myreads/src/SearchBooks.js
@@ -4,6 +4,16 @@ import PropTypes from 'prop-types';
 import * as BooksAPI from './BooksAPI';
 import ListBooks from './ListBooks';
 
+/*
+ * NOTES: The search from BooksAPI is limited to a particular set of search terms.
+ * You can find these search terms here:
+ * https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+ *
+ * However, remember that the BooksAPI.search method DOES search by title or author.
+ * So, don't worry if you don't find a specific author or title. Every search is limited
+ * by search terms.
+ */
+
 class SearchBooks extends Component {
   static propTypes = {
     pageTitle: PropTypes.string.isRequired,
@@ -44,18 +54,11 @@ class SearchBooks extends Component {
             <button className="close-search">Close</button>
           </Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
             <input
               type="text"
               placeholder="Search by title or author"
               value={this.state.query}
+              autoFocus={true}
               onChange={(event) => this.updateQuery(event.target.value)}
             />
           </div>

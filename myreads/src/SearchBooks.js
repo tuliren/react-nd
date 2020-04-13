a73b42c153b38b5165dccfbb93f0c758a25bfe56
Liren Tu <tuliren.git@outlook.com>
2020-04-12 18:52:28 -0700

    Fix search

--- myreads/src/SearchBooks.js
+++ myreads/src/SearchBooks.js
@@ -37,9 +37,11 @@ class SearchBooks extends Component {
 
   updateQuery(query) {
     this.setState({ query });
-    if (this.state.query.length > 0) {
-      BooksAPI.search(this.state.query).then((result) => {
-        if (!result.error) {
+    if (query && query.length > 0) {
+      BooksAPI.search(query.trim()).then((result) => {
+        if (result.error) {
+          this.setState({ books: [] });
+        } else {
           this.setState({ books: result });
         }
       });

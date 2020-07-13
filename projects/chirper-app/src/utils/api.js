a5576aa8796e6f2245978c3d2b6e5b2c4887bae0
Liren Tu <tuliren@gmail.com>
2020-07-12 22:07:45 -0700

    Add chirper app

---
+++ projects/chirper-app/src/utils/api.js
@@ -0,0 +1,19 @@
+import { _getTweets, _getUsers, _saveLikeToggle, _saveTweet } from './_DATA.js';
+
+export function getInitialData() {
+  return Promise.all([
+    _getUsers(),
+    _getTweets(),
+  ]).then(([users, tweets]) => ({
+    users,
+    tweets,
+  }));
+}
+
+export function saveLikeToggle(info) {
+  return _saveLikeToggle(info);
+}
+
+export function saveTweet(info) {
+  return _saveTweet(info);
+}
